b580350aead8292a7bcf1200945105f2b3dae6f4
André Arns <andreluisarns@gmail.com>
2022-05-10 16:29:39 -0300

    Initial Commit

---
+++ Shared/MiniChallenge04_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallenge04_TestApp.swift
+//  Shared
+//
+//  Created by André Arns on 10/05/22.
+//
+
+import SwiftUI
+
+@main
+struct MiniChallenge04_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
